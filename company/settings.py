947c31f273455a86a7ec325ff7952fde8db81d3d
Irina <marf.imom@gmail.com>
2018-12-13 00:05:28 +0200

    My first app, first commit

--- company/settings.py
+++ company/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-fw=hvn9so+*0a6&=-yd(g_a!2(-$v(=0n*xg*arq3lly)29#'
+#SECRET_KEY = '(-fw=hvn9so+*0a6&=-yd(g_a!2(-$v(=0n*xg*arq3lly)29#'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
